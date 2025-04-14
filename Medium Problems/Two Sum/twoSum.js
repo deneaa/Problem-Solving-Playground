0137aa3697115a2613d77eea5adddf0d3ebf5526
Deneaa <denis.stefanw@gmail.com>
2025-04-14 12:10:39 +0300

    Added Two Sum Problem

---
+++ Medium Problems/Two Sum/twoSum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return [];
+};
+
+console.log(twoSum([2,7,11,15], 9)); // [0,1]
+console.log(twoSum([3,2,4], 6)); // [1,2]
+console.log(twoSum([3,3], 6)); // [0,1]
+console.log(twoSum([1,2,3], 4)); // [0,2]
+console.log(twoSum([1,2,3], 5)); // [1,2]
