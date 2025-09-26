9015010adc28090c01af676ea4f1e8269a70c239
Denea <denis.stefanw@gmail.com>
2025-09-26 21:41:28 +0300

    Added Find Minimum in Rotated Sorted Array II Problem

---
+++ Hard Problems/154. Find Minimum in Rotated Sorted Array II/findMin.ts
@@ -0,0 +1,18 @@
+function findMin(nums: number[]): number {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else if (nums[mid] < nums[right]) {
+      right = mid;
+    } else {
+      right--;
+    }
+  }
+
+  return nums[left];
+}
