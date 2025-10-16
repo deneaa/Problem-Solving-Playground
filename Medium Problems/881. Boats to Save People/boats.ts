ec3ee98f0eb9505d9061b19bc0f876972b60c608
Denea <denis.stefanw@gmail.com>
2025-10-16 10:07:24 +0300

    Added Boats to Save People Problem

---
+++ Medium Problems/881. Boats to Save People/boats.ts
@@ -0,0 +1,17 @@
+function numRescueBoats(people: number[], limit: number): number {
+  people.sort((a, b) => a - b);
+  let left = 0;
+  let right = people.length - 1;
+
+  let result = 0;
+  while (left <= right) {
+    if (people[left] + people[right] <= limit) {
+      left++;
+      right--;
+    } else {
+      right--;
+    }
+    result++;
+  }
+  return result;
+}
