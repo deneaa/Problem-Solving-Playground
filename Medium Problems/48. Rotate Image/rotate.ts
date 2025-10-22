d81011b90485469977adc694a3cb019d26b7ac46
Denea <denis.stefanw@gmail.com>
2025-10-22 07:54:57 +0300

    Added Rotate Image Problem

---
+++ Medium Problems/48. Rotate Image/rotate.ts
@@ -0,0 +1,10 @@
+function rotate(matrix: number[][]): void {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i + 1; j < matrix.length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = matrix[i].reverse();
+  }
+}
