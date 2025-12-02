1d8ab0f7bbe2593b31e38b5b07f5295b54c4f3b1
Denea <denis.stefanw@gmail.com>
2025-12-02 21:02:25 +0200

    Added Minimum Absolute Difference in BST Problem

---
+++ Easy Problems/530. Minimum Absolute Difference in BST/minAbsolute.ts
@@ -0,0 +1,21 @@
+function getMinimumDifference(root: TreeNode | null): number {
+  let prev: number | null = null;
+  let minDiff = Infinity;
+
+  function inorder(node: TreeNode | null) {
+    if (!node) return;
+
+    inorder(node.left);
+
+    if (prev !== null) {
+      minDiff = Math.min(minDiff, Math.abs(node.val - prev));
+    }
+
+    prev = node.val;
+
+    inorder(node.right);
+  }
+
+  inorder(root);
+  return minDiff;
+}
